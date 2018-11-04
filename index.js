ef638cba3d0db5fa1a94ca1eb7d970e874fd9a24
Kevin <rakuichijou9@gmail.com>
2018-11-04 21:01:24 +0700

    add index.js learn var, let, and const

---
+++ index.js
@@ -0,0 +1,10 @@
+var message = "hello coding";
+let msg1 = "hello";
+//** const = deklarasi variable konstan = tidak bisa diubah*/
+const msg2 = "coding";
+if (true) {
+    var message = 1;
+    let msg1 = 2;
+}
+console.log(message); 
+console.log(msg1); 
