e940ad4a37b4dcfba5729bf62dff8418cc63eb02
Kevin <rakuichijou9@gmail.com>
2018-11-04 21:26:22 +0700

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var message = "hello coding";
 let msg1 = "hello";
 //** const = deklarasi variable konstan = tidak bisa diubah*/
+//** let = deklarasi variable bersifat private (dalam curly bracketnya) */
 const msg2 = "coding";
 if (true) {
     var message = 1;
