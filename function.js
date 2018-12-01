42b33678d2bb5f2c33c798b8582789c194900364
Kevin <rakuichijou9@gmail.com>
2018-12-01 21:21:34 +0700

    end of Function vid

--- function.js
+++ function.js
@@ -1,9 +1,14 @@
+//!Metode Lama
 function jalanSatu() {
     console.log("jalan satu")
 }
-
 const jalanDua = function() {
     console.log("jalan dua")
 }
+//! Metode Baru
+const jalanTiga = ()=>{
+    console.log("jalan tiga")
+}
 jalanSatu()
 jalanDua()
+jalanTiga()
