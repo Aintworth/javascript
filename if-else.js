a381fedee2e1006c11e03a06c132d15c615b81c0
Kevin <rakuichijou9@gmail.com>
2018-12-01 21:34:09 +0700

    end of if-else

---
+++ if-else.js
@@ -0,0 +1,16 @@
+function jalanSatu()
+{
+    var nilai = 3;
+    if(nilai === 1)
+    {
+        console.log("3");
+    }else if(nilai === 2)
+    {
+        console.log("2");
+    }
+    else
+    {
+        console.log("Salah");
+    }
+}
+jalanSatu();
